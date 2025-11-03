e3c0a877eae3d4e6d1fee2e99af39506a4df0efb
ozk001 <ozk001@stge.org.uk>
2025-11-03 15:50:39 +0000

    First databse lesson

--- Spotify/main.sql
+++ Spotify/main.sql
@@ -1,3 +1,24 @@
-SELECT * 
-FROM Tracks 
-WHERE duration_ms > 200000;
+-- SELECT * 
+-- FROM Artists
+-- WHERE artist_name = 'Lady Gaga';
+
+-- SELECT * 
+-- FROM Tracks;
+
+-- SELECT album_name
+-- FROM Albums
+-- WHERE release_year < 2018;
+
+-- SELECT artist_name FROM Artists;
+-- -- SELECT * FROM Artists;
+-- SELECT * FROM Albums;
+
+-- SELECT * FROM Tracks;
+-- -- WHERE duration_ms > 200000;
+
+-- SELECT album_name FROM Albums;
+
+
+SELECT album_id
+FROM Albums
+WHERE genre_id;

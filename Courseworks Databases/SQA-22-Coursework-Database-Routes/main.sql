98d1b7dc55d9f9422e5521f25105517c8f722731
ozk001 <ozk001@stge.org.uk>
2025-11-18 13:46:54 +0000

    continued 2022 cw

--- Courseworks Databases/SQA-22-Coursework-Database-Routes/main.sql
+++ Courseworks Databases/SQA-22-Coursework-Database-Routes/main.sql
@@ -1,5 +1,20 @@
-SELECT P.forename, P.surname, plannerNO, COUNT W.walkID
-FROM Planner, Walk
-WHERE
-GROUP BY plannerNO
-ORDER BY DESC COUNT W.walkID;
+-- -- 2b
+-- SELECT P.forename, P.surname, P.plannerNO, COUNT (W.walkID)
+-- FROM Planner P
+-- JOIN Route R ON R.plannerNO = P.plannerNO
+-- JOIN Walk W ON W.routeID = R.routeID
+-- GROUP BY P.plannerNO
+-- ORDER BY COUNT (W.walkID) DESC;
+
+-- 2c
+-- SELECT * FROM Walker;
+-- SELECT * FROM Walk;
+-- SELECT * FROM Route;
+-- SELECT * FROM Planner;
+
+SELECT walkerNO, W.forename, W.surname, W.telNO
+FROM Walker W
+JOIN Walk Wa ON W.walkerNO = Wa.walkerNO
+JOIN Route R ON Wa.routeID = R.routeID
+SELECT MAX(distance)
+FROM Route R

d1c2ee3479e48cd24d5fe8e4c35149352fef8386
ozk001 <ozk001@stge.org.uk>
2026-02-03 13:43:20 +0000

    save

--- Courseworks Databases/SQA-Sample-Coursework-Database/main.sql
+++ Courseworks Databases/SQA-Sample-Coursework-Database/main.sql
@@ -1,1 +1,19 @@
-SELECT * FROM Voucher;
+--SELECT V.voucherID, O.custID, C.firstName, C.surname, V.price
+--FROM Voucher V
+--JOIN CustomerOrder O ON O.voucherID = V.voucherID
+--JOIN Customer C ON C.custID = O.custID;
+
+--SELECT C.firstName, C.surname, O.custID
+--FROM Customer C
+--JOIN CustomerOrder O ON O.custID = C.custID;
+
+--SELECT V.price, O.voucherID
+--FROM Voucher V
+--JOIN CustomerOrder O ON O.voucherID = V.voucherID;
+
+SELECT C.firstName, C.surname, V.voucherID, V.price
+FROM Voucher V
+JOIN CustomerOrder O ON O.voucherID = V.voucherID
+JOIN Customer C ON C.custID = O.custID
+GROUP BY category 'Adventure'
+ORDER BY V.voucherID ASC;

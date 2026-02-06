0ea3f54482fef263be79077d4b596d00889d73aa
ozk001 <ozk001@stge.org.uk>
2026-02-06 14:25:17 +0000

    save

--- Courseworks Databases/SQA-20-Coursework-Database-Cars/main.sql
+++ Courseworks Databases/SQA-20-Coursework-Database-Cars/main.sql
@@ -1,1 +1,24 @@
-DESCRIBE ALL TABLES;
+-- DESCRIBE ALL TABLES;
+
+-- SELECT * FROM Car;
+-- SELECT * FROM Garage;
+-- SELECT * FROM Customer;
+-- SELECT * FROM Job;
+
+
+
+-- SELECT G.garageName, SUM(J.cost) AS 'Total Sales'
+-- FROM Garage G
+-- JOIN Job J ON J.garageID = G.garageID
+-- WHERE dateOut LIKE '19-Jan-20'
+-- GROUP BY G.garageName
+-- ORDER BY J.cost ASC;
+
+-- SELECT dateOut
+-- FROM Job
+-- WHERE dateOut LIKE '19-Jan-20';
+
+SELECT , C.regNo, G.garageName
+FROM Garage G
+JOIN Job J ON J.garageID = G.garageID
+JOIN Car C ON C.regNo = J.regNo;

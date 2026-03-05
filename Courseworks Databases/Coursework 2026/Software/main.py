cc9e16dd73b4639e4a34c85fab4af12112a0574c
ozk001 <ozk001@stge.org.uk>
2026-03-05 10:25:21 +0000

    save

--- Courseworks Databases/Coursework 2026/Software/main.py
+++ Courseworks Databases/Coursework 2026/Software/main.py
@@ -1,10 +1,38 @@
 # Software code goes here 
 import csv
 
+#step 1: read file into parallel arrays
 def get_tool_data():
+    tools = []
+    manufacturer = []
+    dateRented = []
+    returned = []
+    fee = []
 
-tool = [] * 120
-manufacturer = [] * 120
-dateRented = [] * 120
-returned = [] * 120
-fee = [] * 120
+    with open('Software/tools.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            tools.append(row[0])
+            manufacturer.append(row[1])
+            dateRented.append(row[2])
+            returned.append(row[3])
+            fee.append(int(row[4]))
+            print()
+    
+#    return tools, manufacturer, dateRented, returned, fee
+
+
+#step 2: display name of tool and total number of tools by a chosen manufacturer
+#def linear_search(manufacturer, TitanForge):
+#    found = False
+#    counter = 0
+#    array_size = len(manufacturer)
+#    while counter < array_size and not found:
+#        if manufacturer[counter] == TitanForge:
+#            found = True
+#        else:
+#            counter += 1
+#    if found:
+#        print(f"{TitanForge} found at {counter}")
+#    else:
+#        print("manufacturer not found")
